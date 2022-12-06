2639b8d1f9b8094c7e728e1dc072a09bc0c296fa
vladiperes <peresvadi@mail.ru>
2022-12-06 21:22:38 +0300

    commit 202212062121 .\Python\HelloyPython\lec.py

--- GB/Python/HelloyPython/lec.py
+++ GB/Python/HelloyPython/lec.py
@@ -2,6 +2,57 @@
 
 
 
+
+# xy = input('Введите через запятую числа, которые будут являться значениями: Х,У. \nХ,У = ').split(",")
+#
+# print(f'Утвеждение, о том, что введенные числа между собой образуют квадрат,\
+# \nлогически соотвествуют: ' +  str(int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0])))
+
+# xy = input('Введите число, которое будет являться значением - Х и затем через запятую, введите число,\
+# \nкоторое будет являться значением - У. ХУ = ').split(",")
+#
+# print(f'Утвеждение, о том, что введенные числа между собой образуют квадрат,\
+# \nлогически соотвествуют: ' +  str(int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# xy = input('Введите значение Х и затем через запятую У : ').split(",")
+# if int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0]):
+#     if int(xy[1]) > int(xy[0]):
+#         print(f'{xy[1]} является квадратом {xy[0]}')
+#     else:
+#         print(f'{xy[0]} является квадратом {xy[1]}')
+# else:
+#     print(f'числа {xy[1]} и {xy[0]} не образуют квадратов')
+
+
+
+
+# xy = input('Введите значение Х и затем через запятую У его квадрат: ').split(",")
+# if int(xy[0])*int(xy[0]) == int(xy[1]):
+#     print(f'{xy[1]} является квадратом {xy[0]}')
+# else:
+#     print(f'{xy[1]} не является квадратом {xy[0]}')
+
+
+
+
+
+# xy = input('Введите значение Х и затем через запятую У: ').split(",")
+# print(xy)
+
+
+
     
     
 
@@ -11,12 +62,12 @@ for line in data:
     print(line)
 data.close() """
 
-with open("/home/vladi/Education/GB/Python/HelloyPython/my/flips_a_number_m", "a") as data:
-    data.write("\original = 23\n")
-    data.write("inverted = 0\n")
-    data.write("while original !=0:\n")
-    data.write("inverted = inverted * 10 + (original%10)\n")
-    data.write("original = original // 10")
+# with open("/home/vladi/Education/GB/Python/HelloyPython/my/split_m", "a") as data:
+#     data.write('\nxy = input("Введите значение Х и затем через запятую У: ").split(",")\n')
+#     data.write("inverted = 0\n")
+#     data.write("while original !=0:\n")
+#     data.write("inverted = inverted * 10 + (original%10)\n")
+#     data.write("original = original // 10")
     
 
 
