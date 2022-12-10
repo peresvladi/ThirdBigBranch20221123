370f67251711e05c0f79858e66313b74f4e66d62
Vladi <peresvladi@nail.com>
2022-12-10 22:21:07 +0300

    commit 202212102220 /home/vladi/Education/GB/Python/HelloyPython

--- GB/Python/HelloyPython/lec.py
+++ GB/Python/HelloyPython/lec.py
@@ -1,90 +1,27 @@
-
-
-
-
-
-# xy = input('Введите через запятую числа, которые будут являться значениями: Х,У. \nХ,У = ').split(",")
-#
-# print(f'Утвеждение, о том, что введенные числа между собой образуют квадрат,\
-# \nлогически соотвествуют: ' +  str(int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0])))
-
-# xy = input('Введите число, которое будет являться значением - Х и затем через запятую, введите число,\
-# \nкоторое будет являться значением - У. ХУ = ').split(",")
-#
-# print(f'Утвеждение, о том, что введенные числа между собой образуют квадрат,\
-# \nлогически соотвествуют: ' +  str(int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0])))
-
-
-
-
-
-
-
-
-
-
-
-
-
-# xy = input('Введите значение Х и затем через запятую У : ').split(",")
-# if int(xy[0])*int(xy[0]) == int(xy[1]) or int(xy[1])*int(xy[1]) == int(xy[0]):
-#     if int(xy[1]) > int(xy[0]):
-#         print(f'{xy[1]} является квадратом {xy[0]}')
-#     else:
-#         print(f'{xy[0]} является квадратом {xy[1]}')
-# else:
-#     print(f'числа {xy[1]} и {xy[0]} не образуют квадратов')
-
-
-
-
-# xy = input('Введите значение Х и затем через запятую У его квадрат: ').split(",")
-# if int(xy[0])*int(xy[0]) == int(xy[1]):
-#     print(f'{xy[1]} является квадратом {xy[0]}')
-# else:
-#     print(f'{xy[1]} не является квадратом {xy[0]}')
-
-
-
-
-
-# xy = input('Введите значение Х и затем через запятую У: ').split(",")
-# print(xy)
-
-
-
+# with open ('/home/vladi/Education/GB/Python/HelloyPython/my/S2/Task/Task2.4', 'a') as data:
+#     data.write('#Задайте список из n элементов  заполненных цифрами из промежутка (-N,N). Найдите произведение элементов на указанных позициях, позиции хранятся в файле file.txt в одной строке одно число\n')
     
-    
+num = int(input('Введите число: '))
+lst = list((range(num*-1,num+1)))
+# with open ('file.py','w') as data:
+#     data.write('def f(x,y):\n')
+#     data.write('    x = x/2\n')
+#     data.write('    return x\n')
+#     data.write('    y = y/2\n')
+#     data.write('    return y\n')
 
-""" path = '/home/vladi/Education/GB/Python/HelloyPython/n_a'
-data = open(path,'r')
-for line in data:
-    print(line)
-data.close() """
 
-# with open("/home/vladi/Education/GB/Python/HelloyPython/my/split_m", "a") as data:
-#     data.write('\nxy = input("Введите значение Х и затем через запятую У: ").split(",")\n')
-#     data.write("inverted = 0\n")
-#     data.write("while original !=0:\n")
-#     data.write("inverted = inverted * 10 + (original%10)\n")
-#     data.write("original = original // 10")
-    
+import file
+print(file.f(num*-1, num+1))
 
 
 
 
 
 
-   
 
 
 
-    
-
-
-
- 
-        
  
 
 
@@ -97,195 +34,6 @@ data.close() """
 
 
 
-
-
-
-
-
-
-
-
-
-            
+    # with open ('/home/vladi/Education/GB/Python/HelloyPython/my/S2/Task/Task2.4', 'a') as data:
+    #     data.write('#Реализуйте механизм нахождения(генерации) рандомного (случайного) числа\n')
     
-          
-        
-
-   
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# count_list1 = [0,0,0]
-# count_list2 = [0,0,1]
-# count_list3 = [0,1,0]
-# count_list4 = [0,1,1]
-# count_list5 = [1,0,0]
-# count_list6 = [1,0,1]
-# count_list7 = [1,1,0]
-# count_list8 = [1,1,1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
