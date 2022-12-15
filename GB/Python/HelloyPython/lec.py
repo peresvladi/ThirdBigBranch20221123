019d62b6ba1ee7412493dd58ff78904a1e0f2d08
vladiperes <peresvadi@mail.ru>
2022-12-15 17:47:55 +0300

    commit 202212151747 .\lec.py

--- GB/Python/HelloyPython/lec.py
+++ GB/Python/HelloyPython/lec.py
@@ -13,17 +13,21 @@ def casual(h):
     a={}
     d=[]
     s=0
+    t=0
     b=(list(str(id(a))))
     b=b[3:len(b)] # значение первых трех элементов исключаем т.к. их значения при вызовах id существенно не обнавляются
     if h < 10:
         b
-        print(f'type : b {type(b)}')
+
     elif 9 < h < 100:
         for j in b:
-            d=d + [b[s-1]+j]
-            s=s+1
-        b=b+d
-        print(f'type : b {type(b)}')
+            d = d + [b[s - 1] + j]
+            s = s + 1
+
+        for y in b:
+            d.insert(t, y)
+            t = t + 2
+        b = d
     return b
 
 def ccontrole(m_n, m_x, c_t):
@@ -34,7 +38,7 @@ def ccontrole(m_n, m_x, c_t):
         result = ssum[len(ssum) - count : len(ssum)]
     return result 
     
-count = 3
+count = 10
 max = 14
 min = 0
 print (ccontrole (min, max, count))
